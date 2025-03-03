177d76eed7487beb232e0ef95e62090b346549d9
Thiniraxz <thiniragenuka@gmail.com>
2025-03-03 21:53:54 +0530

    Fix error message when permissions are insufficient

--- packages/spatial-id-viewer/src/components/area-creator/interfaces.ts
+++ packages/spatial-id-viewer/src/components/area-creator/interfaces.ts
@@ -1,4 +1,4 @@
 export const errorMessages: { [key: string]: string } = {
   ErrorCode_Duplicate: '重複エラーが発生しました。',
-  ErrorCode_AccessDeny: 'アクセスが拒否されました。ログインしてもう一度お試しください。',
+  ErrorCode_AccessDeny: 'この操作を実行する権限がありません。管理者に問い合わせください',
 };
